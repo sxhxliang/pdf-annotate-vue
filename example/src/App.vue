6f95f7000f77d5c0910d38dda0206723c9169d48
AaronLeong <626786864@qq.com>
2019-05-31 17:05:32 +0800

    add example

---
+++ example/src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+     pdf path: {{pdf}}
+    <pdfAnnotate :src="pdf" :toolbar="toolbar" :commentWrapper="commentWrapper"></pdfAnnotate>
+  </div>
+</template>
+
+<script>
+
+import pdfAnnotate from 'pdf-annotate-vue'
+import "pdf-annotate-vue/src/css/toolbar.css";
+import "pdf-annotate-vue/src/css/pdf_viewer.css";
+
+
+export default {
+  name: 'home',
+  components: {
+    pdfAnnotate
+  },
+  data(){
+    return {
+      pdf: './static/output.pdf',
+      toolbar: true,
+      commentWrapper: true,
+    }
+  }
+}
+</script>
+
+
+
